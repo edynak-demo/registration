4805cee0c73eb0a76e2cb8dbd4ae7884549bc9d8
edynak-demo <edynakdemo@gmail.com>
2020-09-13 13:05:49 -0400

    implemented user registration

--- signup.php
+++ signup.php
@@ -1,6 +1,30 @@
 <?php 
 
-include_once 'system/Database.php'
+include_once 'system/Database.php';
+
+if(isset($_POST['email'])){
+  $email = $_POST['email'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+  try{
+    $sqlInsert = "INSERT INTO users (username, email, password, signup_date) VALUES (:username, :email, :password, now())";
+
+    $statement = $db->prepare($sqlInsert);
+    $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+
+    if($statement->rowCount() == 1){
+      $result = "<p style='padding: 20px; color: green;'> Registration Succesful </p>";
+    }
+
+  }catch (PDOException $ex){
+    $result = "<p style='padding: 20px; color: red;'> An Error Has Occurred: ".$ex->getMessage()."</p>";
+  }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,11 +39,13 @@ include_once 'system/Database.php'
 
   <h3>Registration Form</h3>
 
+  <?php if(isset($result)) echo $result; ?>
+
   <form method="post" action="">
     <table>
-      <tr><td>Email:</td> <td><input type="text" value=""></td></tr>
-      <tr><td>Username:</td> <td><input type ="text" value=""></td></tr>
-      <tr><td>Password:</td> <td><input type="password" value=""></td></tr>
+      <tr><td>Email:</td> <td><input type="text" value="" name="email"></td></tr>
+      <tr><td>Username:</td> <td><input type ="text" value="" name="username"></td></tr>
+      <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
       <tr><td></td><td><input style="float: right;" type="submit" value="Sign In"></td></tr>
     </table>
   </form>
