d1cee62bc54c72e2ec8db66fa4fa8255c47fdc85
edynak-demo <edynakdemo@gmail.com>
2020-09-13 16:06:03 -0400

    implemented form validation and error handling

--- signup.php
+++ signup.php
@@ -1,36 +1,64 @@
 <?php 
 // db connection script
 include_once 'system/Database.php';
+include_once 'system/utilities.php';
 
-// check if error array is empty, if yes process form data and insert record
-if(isset($_POST['email'])){
-  // collect form data and store in variables
-  $email = $_POST['email'];
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+// process the form
+if(isset($_POST['signupBtn'])){
+    // initialize an array to store any error message from the form
+    $form_errors = array();
 
-  // hashing the password
-  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    // Form validation
+    $required_fields = array('email', 'username', 'password');
 
-  try{
+    // call the function to check empty field and merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
 
-    // create SQL insert statement
-    $sqlInsert = "INSERT INTO users (username, email, password, signup_date) VALUES (:username, :email, :password, now())";
+    // Fields that requires checking for minimum length
+    $fields_to_check_length = array('username' => 4, 'password' => 6);
 
-    // use PDO prepared to sanitize data
-    $statement = $db->prepare($sqlInsert);
+    // call the function to check minimum required length and merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_min_length($fields_to_check_length));
 
-    // add the data into the database
-    $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+    // email validation / merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_email($_POST));
 
-    // check if one new row created in db
-    if($statement->rowCount() == 1){
-      $result = "<p style='padding: 20px; color: green;'> Registration Succesful </p>";
+    // check if error array is empty, if yes process form data and insert record
+    if(empty($form_errors)){
+        // collect form data and store in variables
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // hashing the password
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        try{
+
+            // create SQL insert statement
+            $sqlInsert = "INSERT INTO users (username, email, password, signup_date)
+              VALUES (:username, :email, :password, now())";
+
+            // use PDO prepared to sanitize data
+            $statement = $db->prepare($sqlInsert);
+
+            // add the data into the database
+            $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+
+            // check if one new row was created
+            if($statement->rowCount() == 1){
+                $result = "<p style='padding:20px; border: 1px solid gray; color: green;'> Registration Successful</p>";
+            }
+        }catch (PDOException $ex){
+            $result = "<p style='padding:20px; border: 1px solid gray; color: red;'> An error occurred: ".$ex->getMessage()."</p>";
+        }
     }
-
-  }catch (PDOException $ex){
-    $result = "<p style='padding: 20px; color: red;'> An Error Has Occurred: ".$ex->getMessage()."</p>";
-  }
+    else{
+        if(count($form_errors) == 1){
+            $result = "<p style='color: red;'> There was 1 error in the form<br>";
+        }else{
+            $result = "<p style='color: red;'> There were " .count($form_errors). " errors in the form <br>";
+        }
+    }
 
 }
 
@@ -49,17 +77,16 @@ if(isset($_POST['email'])){
   <h3>Registration Form</h3>
 
   <?php if(isset($result)) echo $result; ?>
+  <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
 
   <form method="post" action="">
     <table>
       <tr><td>Email:</td> <td><input type="text" value="" name="email"></td></tr>
       <tr><td>Username:</td> <td><input type ="text" value="" name="username"></td></tr>
       <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
-      <tr><td></td><td><input style="float: right;" type="submit" value="Sign Up"></td></tr>
+      <tr><td></td><td><input style="float: right;" type="submit" name= "signupBtn" value="Sign Up"></td></tr>
     </table>
   </form>
-
-
   <p><a href="index.php">Back</a></p>
 </body>
 </html>
