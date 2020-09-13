99fa5e5100601ab8b46dab66279e0aa19aada4fb
edynak-demo <edynakdemo@gmail.com>
2020-09-13 15:17:36 -0400

    added comments to signup.php

--- signup.php
+++ signup.php
@@ -1,20 +1,29 @@
 <?php 
-
+// db connection script
 include_once 'system/Database.php';
 
+// check if error array is empty, if yes process form data and insert record
 if(isset($_POST['email'])){
+  // collect form data and store in variables
   $email = $_POST['email'];
   $username = $_POST['username'];
   $password = $_POST['password'];
 
+  // hashing the password
   $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
   try{
+
+    // create SQL insert statement
     $sqlInsert = "INSERT INTO users (username, email, password, signup_date) VALUES (:username, :email, :password, now())";
 
+    // use PDO prepared to sanitize data
     $statement = $db->prepare($sqlInsert);
+
+    // add the data into the database
     $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
 
+    // check if one new row created in db
     if($statement->rowCount() == 1){
       $result = "<p style='padding: 20px; color: green;'> Registration Succesful </p>";
     }
@@ -46,7 +55,7 @@ if(isset($_POST['email'])){
       <tr><td>Email:</td> <td><input type="text" value="" name="email"></td></tr>
       <tr><td>Username:</td> <td><input type ="text" value="" name="username"></td></tr>
       <tr><td>Password:</td> <td><input type="password" value="" name="password"></td></tr>
-      <tr><td></td><td><input style="float: right;" type="submit" value="Sign In"></td></tr>
+      <tr><td></td><td><input style="float: right;" type="submit" value="Sign Up"></td></tr>
     </table>
   </form>
 
--- system/Database.php
+++ system/Database.php
@@ -1,16 +1,20 @@
 <?php 
-
+// initialize variables to hold connection parameters
 $username = 'root';
 $dsn = 'mysql:host=localhost; dbname=registration';
 $password = '';
 
 try{
+  // create an instance of the PDO class with the required parameters
   $db = new PDO($dsn, $username, $password);
 
+  // set pdo error mode to exception
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  echo "Connected to your db";
+  //display success message
+  // echo "Connected to your db";
 }catch (PDOException $ex){
+  // display error message
   echo "Connection to db failed".$ex->getMessage();
 }
 ?>
